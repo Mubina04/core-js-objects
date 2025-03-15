94befbf8adfdd01562fe826f3f0319cd6f5a1053
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2025-03-15 10:16:41 +0500

    task: in progress

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,9 +17,7 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
-}
+const shallowCopy = (obj) => ({ ...obj });
 
 /**
  * Merges array of objects into a single object. If there are overlapping keys, the values
@@ -32,8 +30,13 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((acc, obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      acc[key] = (acc[key] || 0) + value;
+    });
+    return acc;
+  }, {});
 }
 
 /**
@@ -49,8 +52,10 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = { ...obj }; // Create a shallow copy
+  keys.forEach((key) => delete newObj[key]); // Remove specified properties
+  return newObj;
 }
 
 /**
@@ -65,8 +70,17 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  return keys1.every(
+    (key) => Object.hasOwn(obj2, key) && obj1[key] === obj2[key]
+  );
 }
 
 /**
@@ -80,8 +94,12 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  const keys1 = Object.keys(obj);
+  if (!keys1.length) {
+    return true;
+  }
+  return false;
 }
 
 /**
